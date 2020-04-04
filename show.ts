d3d21f39f379968455da76c1b587091c3209e154
Dave Compton <davecompton7@gmail.com>
2020-04-04 13:13:09 -0700

    changed Title to link

--- show.ts
+++ show.ts
@@ -1,42 +1,20 @@
 declare var PR:any; 
 
-function getOnlineText(titleId:string, contentId:string):Promise<void>
+function getOnlineText(titleId:string, contentId:string)
 {
-  // get the URL to display and determine how to display it.
   let params = new URLSearchParams(window.location.search);  
-  let urlParam:string;
-  let showAsCode:boolean;
 
-  if (params.has("codeURL")) {
-    urlParam = "codeURL";  
-    showAsCode = true;
-  } else {
-    if (params.has("textURL")) {
-      urlParam = "textURL";  
-      showAsCode = false;
-    } else {
-      console.log ("Expecting either textURL or codeURL as argument");
-      return;
-    }
-  }
-
-  let encodedUrl = params.get(urlParam);  
+  let encodedUrl = params.get("codeURL");  
   let url = decodeURIComponent(encodedUrl);
 
   // display the url
   document.getElementById(titleId).innerText = url;
+  (<HTMLAnchorElement>document.getElementById(titleId)).href = url;
 
   // display the content at the url
   let el = document.getElementById(contentId);
   fetch(url)
     .then(resp => resp.text())
-    .then(resp => { if (showAsCode) {
-                      el.innerHTML = PR.prettyPrintOne(resp);  
-                    } else {
-                      el.innerText = resp; 
-                    }
-                    return;
-                  }
-         )
-    .catch(err => { el.innerText = err; })
+    .then(resp => el.innerHTML = PR.prettyPrintOne(resp.replace(/</g,"&lt").replace(/>/g,"&gt")))
+    .catch(err => { el.innerText = err; });
 }
