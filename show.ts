0b7c0f8fb944bf4ff363a97e425115fbb7f7b4da
Dave Compton <davecompton7@gmail.com>
2020-04-05 22:30:51 +0000

    simplified ; removed title from page

--- show.ts
+++ show.ts
@@ -1,15 +1,11 @@
 declare var PR:any; 
 
-function getOnlineText(titleId:string, contentId:string)
+function getOnlineText(contentId:string)
 {
   let params = new URLSearchParams(window.location.search);  
 
   let encodedUrl = params.get("codeURL");  
   let url = decodeURIComponent(encodedUrl);
-
-  // display the url
-  document.getElementById(titleId).innerText = url;
-  (<HTMLAnchorElement>document.getElementById(titleId)).href = url;
 
   // display the content at the url
   let el = document.getElementById(contentId);
