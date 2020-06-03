dbf6da851f537910ac86b05c2fafd9624c005cfb
Dave Compton <davecompton7@gmail.com>
2020-06-03 15:26:36 -0700

    moved param reading into html

--- show.ts
+++ show.ts
@@ -1,12 +1,7 @@
 declare var PR:any; 
 
-function getOnlineText(displayCB: (st: string) => void)
+function getOnlineText(url: string, displayCB: (st: string) => void)
 {
-  let params = new URLSearchParams(window.location.search);  
-
-  let encodedUrl = params.get("codeURL");  
-  let url = decodeURIComponent(encodedUrl);
-
   fetch(url)
     .then(resp => resp.text())
     .then(resp => displayCB(resp))
