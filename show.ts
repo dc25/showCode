0f952bf8d5ff285365e4f1a5724e03cb0a2767cc
Dave Compton <davecompton7@gmail.com>
2020-06-03 14:57:44 -0700

    passing callback function to getOnlineText instead of string identifying element to update

--- show.ts
+++ show.ts
@@ -1,24 +1,14 @@
 declare var PR:any; 
 
-function getOnlineText(contentId:string)
+function getOnlineText(displayCB: (st: string) => void)
 {
   let params = new URLSearchParams(window.location.search);  
 
   let encodedUrl = params.get("codeURL");  
   let url = decodeURIComponent(encodedUrl);
 
-  // display the content at the url
-  let el = document.getElementById(contentId);
   fetch(url)
     .then(resp => resp.text())
-    .then(resp => el.innerHTML 
-                      = PR.prettyPrintOne(
-                            resp.replace(/&/g,"&amp;")
-                                .replace(/</g,"&lt")
-                                .replace(/>/g,"&gt")
-                          , null // let prettyPrintOne figure out the language
-                          , true // print line numbers.
-                          )
-         )
-    .catch(err => el.innerText = err);
+    .then(resp => displayCB(resp))
+    .catch(err => displayCB(err));
 }
