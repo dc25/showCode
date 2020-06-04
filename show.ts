df0e3ed4d4ee23c0ed1661581a4358aaea365839
Dave Compton <davecompton7@gmail.com>
2020-06-03 17:03:32 -0700

    calling prettyPrintOne now

--- show.ts
+++ show.ts
@@ -1,9 +1,23 @@
 declare var PR:any; 
 
-function getOnlineText(url: string, displayCB: (st: string) => void)
+export function getOnlineText(url: string, displayCB: (st: string) => void)
 {
   fetch(url)
     .then(resp => resp.text())
-    .then(resp => displayCB(resp))
+    .then(resp => { let replaced 
+                          = resp.replace(/&/g,"&amp;")
+                                .replace(/</g,"&lt")
+                                .replace(/>/g,"&gt");
+
+                    let prettied 
+                          = PR.prettyPrintOne
+                              ( replaced
+                              , null // let prettyPrintOne guess the language
+                              , true // print line numbers.
+                              );
+
+                    displayCB(prettied); 
+                  }
+         )
     .catch(err => displayCB(err));
 }
