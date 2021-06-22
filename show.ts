71c298a301576a0eed6c0147d914bcc862cf5227
Dave Compton <davecompton7@gmail.com>
2021-06-22 13:42:04 +0000

    using async/await syntax

--- show.ts
+++ show.ts
@@ -1,32 +1,30 @@
 declare var PR:any; 
 
-function getOnlineText(search: string, displayCB: (st: string) => void) : void
+async function getOnlineText(search: string, displayCB: (st: string) => any) 
 {
-  let params = new URLSearchParams(search);  
-  let encodedUrl = params.get("codeURL");  
-  if (encodedUrl === null) {
-     displayCB("Missing argument 'codeURL' in query params: \'" + search + "\'.");
-     return;
+  try {
+    const params = new URLSearchParams(search);  
+    const encodedUrl = params.get("codeURL");  
+    if (encodedUrl === null) {
+       throw "Missing argument 'codeURL' in query params: \'" + search + "\'.";
+    }
+
+    const url = decodeURIComponent(encodedUrl);
+
+    const resp = await fetch(url);
+    const respText = await resp.text();
+    const replaced = respText.replace(/&/g,"&amp;")
+                             .replace(/</g,"&lt")
+                             .replace(/>/g,"&gt");
+
+    const prettied = PR.prettyPrintOne
+                                ( replaced
+                                , null // let prettyPrintOne guess the language
+                                , true // print line numbers.
+                                );
+
+    displayCB(prettied); 
+  } catch (err) {
+    displayCB(err);
   }
-
-  let url = decodeURIComponent(encodedUrl);
-
-  fetch(url)
-    .then(resp => resp.text())
-    .then(resp => { let replaced 
-                          = resp.replace(/&/g,"&amp;")
-                                .replace(/</g,"&lt")
-                                .replace(/>/g,"&gt");
-
-                    let prettied 
-                          = PR.prettyPrintOne
-                              ( replaced
-                              , null // let prettyPrintOne guess the language
-                              , true // print line numbers.
-                              );
-
-                    displayCB(prettied); 
-                  }
-         )
-    .catch(err => displayCB(err));
 }
